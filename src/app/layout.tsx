034498d0c74c55c979fa6fd769229e0ce40daf2a
Abhirup Ghosh <codewithabhi2024@gmail.com>
2025-06-14 15:30:27 +0530

    integrate db and studio videos

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import { TRPCProvider } from "@/trpc/client";
+import { Toaster } from "@/components/ui/sonner";
 const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -19,6 +20,7 @@ export default function RootLayout({
       <html lang="en" >
         <body className={inter.className}>
           <TRPCProvider>
+            <Toaster/>
           {children}
           </TRPCProvider>
           </body>
--- src/db/schema.ts
+++ src/db/schema.ts
@@ -5,7 +5,7 @@ import {
   timestamp,
   uniqueIndex,
 } from "drizzle-orm/pg-core";
-
+import { relations } from "drizzle-orm";
 export const users = pgTable(
   "users",
   {
@@ -20,10 +20,53 @@ export const users = pgTable(
   (t) => [uniqueIndex("clerk_id_idx").on(t.clerkId)]
 );
 
-export const categories = pgTable("categories", {
+export const categories = pgTable(
+  "categories",
+  {
+    id: uuid("id").primaryKey().defaultRandom(),
+    name: text("name").notNull().unique(),
+    description: text("description"),
+    createdAt: timestamp("created_at").defaultNow().notNull(),
+    updatedAt: timestamp("updated_at").defaultNow().notNull(),
+  },
+  (t) => [uniqueIndex("name_idx").on(t.name)]
+);
+
+
+
+
+
+export const videos = pgTable("videos", {
   id: uuid("id").primaryKey().defaultRandom(),
-  name: text("name").notNull().unique(),
+  title: text("title").notNull(),
   description: text("description"),
+  userId: uuid("user_id")
+    .references(() => users.id, {
+      onDelete: "cascade",
+    })
+    .notNull(),
+  categoryId: uuid("category_id").references(()=> categories.id,{
+      onDelete: "set null",
+    }),
   createdAt: timestamp("created_at").defaultNow().notNull(),
   updatedAt: timestamp("updated_at").defaultNow().notNull(),
-},(t)=> [uniqueIndex("name_idx").on(t.name)]);
+});
+
+export const videoRelations = relations(videos, ({ one }) => ({
+  user: one(users, {
+    fields: [videos.userId],
+    references: [users.id],
+  }),
+  category: one(categories, {
+    fields: [videos.categoryId],
+    references: [categories.id]
+  })
+}));
+
+export const userRelations = relations(users, ({ many }) => ({
+  videos: many(videos),
+}));
+
+export const categoryRelations = relations(users, ({ many }) => ({
+  videos: many(videos),
+}));
