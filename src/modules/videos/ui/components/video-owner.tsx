b10583c09e29df106d9f48f6dffc939f32a8167e
Abhirup Ghosh <codewithabhi2024@gmail.com>
2025-06-16 11:03:59 +0530

    video page updates

---
+++ src/modules/videos/ui/components/video-owner.tsx
@@ -0,0 +1,44 @@
+import Link from "next/link";
+import { VideoGetOneOutput } from "../../types";
+import { UserAvatar } from "@/components/user-avatar";
+import { useAuth } from "@clerk/nextjs";
+import { Button } from "@/components/ui/button";
+import { SubscriptionButton } from "@/modules/subscriptions/ui/components/subscription-button";
+import { UserInfo } from "@/modules/users/ui/components/user-info";
+
+interface VideoOwnerProps {
+  user: VideoGetOneOutput["user"];
+  videoId: string;
+}
+
+export const VideoOwner = ({ user, videoId }: VideoOwnerProps) => {
+  const { userId: clerkUserId } = useAuth();
+
+  return (
+    <div className="flex items-center sm:items-start justify-between sm:justify-start gap-3 min-w-0">
+      <Link href={`/users/${user.id}`}>
+        <div className="flex items-center gap-3 min-w-0">
+          <UserAvatar size="lg" imageUrl={user.imageUrl} name={user.name} />
+          <div className="flex flex-col gap-1 min-w-0">
+            <UserInfo size="lg" name={user.name} />
+            <span className="text-sm text-muted-foreground line-clamp-1">
+              {4} Subscribers
+            </span>
+          </div>
+        </div>
+      </Link>
+      {clerkUserId === user.clerkId ? (
+        <Button>
+          <Link href={`/studio/videos/${videoId}`}>Edit Video</Link>
+        </Button>
+      ) : (
+        <SubscriptionButton
+          disabled={false}
+          onClick={() => {}}
+          isSubscribed={false}
+          className="flex-none"
+        />
+      )}
+    </div>
+  );
+};
---
+++ src/modules/videos/ui/sections/video-section.tsx
@@ -0,0 +1,48 @@
+"use client";
+
+import { cn } from "@/lib/utils";
+import { trpc } from "@/trpc/client";
+import { Suspense } from "react";
+import { ErrorBoundary } from "react-error-boundary";
+import { VideoPlayer } from "../components/video-player";
+import { VideoBanner } from "../components/video-banner";
+import { VideoTopRow } from "../components/video-top-row";
+
+interface videoSectionProps {
+  videoId: string;
+}
+
+export const VideoSection = ({ videoId }: videoSectionProps) => {
+  return (
+    <Suspense fallback={<p>Loading...</p>}>
+      <ErrorBoundary fallback={<p>Error!</p>}>
+        <VideoSectionSuspense videoId={videoId} />
+      </ErrorBoundary>
+    </Suspense>
+  );
+};
+
+const VideoSectionSuspense = ({ videoId }: videoSectionProps) => {
+  const [video] = trpc.videos.getOne.useSuspenseQuery({ id: videoId });
+
+  return (
+    <>
+      <div
+        className={cn(
+          "aspect-video bg-black rounded-2xl overflow-hidden relative",
+          video.muxStatus !== "ready" && "rounded-b-none"
+        )}
+      >
+        <VideoPlayer
+          autoPlay
+          onPlay={() => {}}
+          playbackId={video.muxPlaybackId}
+          thumbnailUrl={video.thumbnailUrl}
+        />
+      </div>
+      <VideoBanner status = {video.muxStatus}/>
+      <VideoTopRow video={video}/>
+    </>
+  );
+};
+ 
