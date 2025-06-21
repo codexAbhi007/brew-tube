a87687f7fe4ae96d1024236d54efca1114993dff
Abhirup Ghosh <codewithabhi2024@gmail.com>
2025-06-21 08:45:51 +0530

    add playlist page and upload modal

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -124,6 +124,7 @@ export const videoRelations = relations(videos, ({ one, many }) => ({
   views: many(videoViews),
   reactions: many(videoReactions),
   comments: many(comments),
+  playlistVideos: many(playlistVideos),
 }));
 
 export const userRelations = relations(users, ({ many }) => ({
@@ -138,6 +139,7 @@ export const userRelations = relations(users, ({ many }) => ({
   }),
   comments: many(comments),
   commentReactions: many(commentReactions),
+  playlists: many(playlists)
 }));
 
 export const categoryRelations = relations(users, ({ many }) => ({
@@ -255,12 +257,12 @@ export const commentRelations = relations(comments, ({ one, many }) => ({
   parent: one(comments, {
     fields: [comments.parentId],
     references: [comments.id],
-    relationName: "comments_parent_id_fkey"
+    relationName: "comments_parent_id_fkey",
   }),
   reactions: many(commentReactions),
   replies: many(comments, {
-    relationName: "comments_parent_id_fkey"
-  })
+    relationName: "comments_parent_id_fkey",
+  }),
 }));
 
 export const commentSelectSchema = createSelectSchema(comments);
@@ -301,3 +303,53 @@ export const commentReactionRelations = relations(
     }),
   })
 );
+
+export const playlists = pgTable("playlists", {
+  id: uuid("id").primaryKey().defaultRandom(),
+  name: text("name").notNull(),
+  description: text("description"),
+  userId: uuid("user_id")
+    .references(() => users.id, { onDelete: "cascade" })
+    .notNull(),
+  createdAt: timestamp("created_at").defaultNow().notNull(),
+  updatedAt: timestamp("updated_at").defaultNow().notNull(),
+});
+
+export const playlistVideos = pgTable(
+  "playlist_videos",
+  {
+    playlistId: uuid("playlist_id")
+      .references(() => videos.id, { onDelete: "cascade" })
+      .notNull(),
+    videoId: uuid("video_id")
+      .references(() => videos.id, { onDelete: "cascade" })
+      .notNull(),
+    createdAt: timestamp("created_at").defaultNow().notNull(),
+    updatedAt: timestamp("updated_at").defaultNow().notNull(),
+  },
+  (t) => [
+    primaryKey({
+      name: "playlist_videos_pk",
+      columns: [t.playlistId, t.videoId],
+    }),
+  ]
+);
+
+export const playlistVideoRelations = relations(playlistVideos, ({ one }) => ({
+  playlist: one(playlists, {
+    fields: [playlistVideos.playlistId],
+    references: [playlists.id],
+  }),
+  video: one(videos, {
+    fields: [playlistVideos.videoId],
+    references: [videos.id],
+  }),
+}));
+
+export const playlistRelations = relations(playlists, ({ one, many }) => ({
+  user: one(users, {
+    fields: [playlists.userId],
+    references: [users.id],
+  }),
+  playlistVideos: many(playlistVideos),
+}));
---
+++ src/modules/playlists/ui/sections/playlists-section.tsx
@@ -0,0 +1,64 @@
+"use client";
+
+import { InfiniteScroll } from "@/components/infinite-scroll";
+import { DEFAULT_LIMIT } from "@/constants";
+import {
+  
+  VideoGridCardSkeleton,
+} from "@/modules/videos/ui/components/video-grid-card";
+import {VideoRowCardSkeleton } from "@/modules/videos/ui/components/video-row-card";
+import { trpc } from "@/trpc/client";
+import { Suspense } from "react";
+import { ErrorBoundary } from "react-error-boundary";
+
+export const PlaylistsSection = () => {
+  return (
+    <Suspense fallback={<PlaylistsSectionSkeleton />}>
+      <ErrorBoundary fallback={<p>Error</p>}>
+        <PlaylistsSectionSuspense />
+      </ErrorBoundary>
+    </Suspense>
+  );
+};
+
+const PlaylistsSectionSkeleton = () => {
+  return (
+    <div>
+      <div className="flex flex-col gap-4 gap-y-10 md:hidden">
+        {Array.from({ length: 20 }).map((_, index) => (
+          <VideoGridCardSkeleton key={index} />
+        ))}
+      </div>
+      <div className="hidden flex-col gap-4  md:flex">
+        {Array.from({ length: 20 }).map((_, index) => (
+          <VideoRowCardSkeleton key={index} size="compact"/>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+const PlaylistsSectionSuspense = () => {
+  const [playlists, query] = trpc.playlists.getMany.useSuspenseInfiniteQuery(
+    {
+      limit: DEFAULT_LIMIT,
+    },
+    {
+      getNextPageParam: (lastPage) => lastPage.nextCursor,
+    }
+  );
+
+  return (
+    <div>
+      <div className="flex flex-col gap-4 gap-y-10 ">
+        {JSON.stringify(playlists)}
+      </div>
+    
+      <InfiniteScroll
+        hasNextPage={query.hasNextPage}
+        isFetchingNextPage={query.isFetchingNextPage}
+        fetchNextPage={query.fetchNextPage}
+      />
+    </div>
+  );
+};
